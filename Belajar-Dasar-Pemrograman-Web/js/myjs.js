213d8095161bbc81526f44f762473c7fbde6f200
@code <jj4068851@gmail.com>
2023-09-15 09:38:53 +0700

    Add: aside semantic

--- Belajar-Dasar-Pemrograman-Web/js/myjs.js
+++ Belajar-Dasar-Pemrograman-Web/js/myjs.js
@@ -14,3 +14,13 @@ window.onclick = event => {
         }
     }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
