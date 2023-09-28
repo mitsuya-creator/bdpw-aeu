7423b890561120852ff4906ec4aa86d86cf5a4bc
@code <jj4068851@gmail.com>
2023-09-28 08:07:34 +0700

    Add: add logic to avoid duplicate card

--- Membuat-Front-End-Web-Pemula/bookshelfApps/js/script.js
+++ Membuat-Front-End-Web-Pemula/bookshelfApps/js/script.js
@@ -17,6 +17,20 @@ window.addEventListener("load", function () {
                 let book = generateBook(id, titleBook.value, writer.value, yearBooksInt, isRead.checked);
                 listBooks.push(book);
                 resetValue(titleBook, writer, isRead, yearBooks);
+                const listRead = document.getElementById("list-read");
+                const listUnRead = document.getElementById("list-unread");
+                if (listUnRead.hasChildNodes()) listUnRead.innerHTML = "";
+                if (listRead.hasChildNodes()) listRead.innerHTML = "";
+                let todoElement;
+                listBooks.forEach(content => {
+                    if (content.isComplete) {
+                        todoElement = createCard(content.title, content.author, content.year, content.isComplete);
+                        listRead.append(todoElement);
+                    } else {
+                        todoElement = createCard(content.title, content.author, content.year, content.isComplete);
+                        listUnRead.append(todoElement);
+                    }
+                })
                 console.log(listBooks);
             }
         })
@@ -64,32 +78,26 @@ function generateBook(id, title, author, year, isComplete) {
     }
 }
 
-// function createCard(titleBook, writer, year, isRead) {
-//     let cardBook = document.createElement("div");
-//     cardBook.classList.add("card-book");
-//     cardBook.innerHTML = `<h2>${titleBook}</h2>
-//                         <div class="details">
-//                             <span>${writer}</span>
-//                             <span>${year}</span>
-//                         </div>`;
-//     let containerButtonAction = document.createElement("div");
-//     containerButtonAction.classList.add("container-button-actions");
-//     let isReadSpan;
-//     if (isRead === true) {
-//         isReadSpan = "Belum dibaca";
-//     } else {
-//         isReadSpan = "Sudah dibaca";
-//     }
-//     containerButtonAction.innerHTML = `<button type="button" class="btn-succes">${isReadSpan}</button>
-//                             <button type="button" class="btn-danger">Hapus buku</button>`;
-//     cardBook.appendChild(containerButtonAction);
-//     if (isRead === true) {
-//         let listRead = document.getElementById("list-read");
-//         listRead.appendChild(cardBook);
-//     } else {
-//         let listUnRead = document.getElementById("list-unread");
-//         listUnRead.appendChild(cardBook)
-//     }
+function createCard(titleBook, writer, year, isRead) {
+    let cardBook = document.createElement("div");
+    cardBook.classList.add("card-book");
+    cardBook.innerHTML = `<h2>${titleBook}</h2>
+                        <div class="details">
+                            <span>${writer}</span>
+                            <span>${year}</span>
+                        </div>`;
+    let containerButtonAction = document.createElement("div");
+    containerButtonAction.classList.add("container-button-actions");
+    let isReadSpan;
+    if (isRead === true) {
+        isReadSpan = "Belum dibaca";
+    } else {
+        isReadSpan = "Sudah dibaca";
+    }
+    containerButtonAction.innerHTML = `<button type="button" class="btn-succes">${isReadSpan}</button>
+                            <button type="button" class="btn-danger">Hapus buku</button>`;
+    cardBook.appendChild(containerButtonAction);
 
-// }
+    return cardBook;
+}
 const listBooks = [];
